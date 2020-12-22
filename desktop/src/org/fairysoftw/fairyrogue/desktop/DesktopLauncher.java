266cb3feb440f7379bdf5fa5132c0015ca2b48bc
colourfulblack <kalicock@outlook.com>
2020-12-23 02:27:20 +0800

    change to libgdx

---
+++ desktop/src/org/fairysoftw/fairyrogue/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.fairysoftw.fairyrogue.desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import org.fairysoftw.fairyrogue.FairyRogue;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new FairyRogue(), config);
+	}
+}
