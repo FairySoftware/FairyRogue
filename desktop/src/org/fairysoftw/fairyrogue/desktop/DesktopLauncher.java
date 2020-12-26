d77b1e2abbd49b8d2242dbc4a0e2dfffc4b01cf7
colourfulblack <kalicock@outlook.com>
2020-12-27 07:34:12 +0800

    solve blink problem by diable vsync

--- desktop/src/org/fairysoftw/fairyrogue/desktop/DesktopLauncher.java
+++ desktop/src/org/fairysoftw/fairyrogue/desktop/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setWindowedMode(FairyRogue.VIRTUAL_WIDTH, FairyRogue.VIRTUAL_HEIGHT);
+		config.useVsync(false);
 		new Lwjgl3Application(new FairyRogue(), config);
 	}
 }
