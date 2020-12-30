dcdd3db31b94b3f808d1ee0eb0998fa185c432c6
colourfulblack <kalicock@outlook.com>
2020-12-30 14:58:53 +0800

    add PlayerActor comment

--- core/src/org/fairysoftw/fairyrogue/actor/PlayerActor.java
+++ core/src/org/fairysoftw/fairyrogue/actor/PlayerActor.java
@@ -16,6 +16,10 @@ import org.fairysoftw.fairyrogue.props.Props;
 import org.fairysoftw.fairyrogue.screen.MainScreen;
 import org.json.JSONObject;
 
+/** {@inheritDoc}
+ * actor that inherit creature actor
+ * represent player actor
+ */
 public class PlayerActor extends CreatureActor {
     private Vector2 lastPosition = new Vector2();
     public Array<Props> backpack = new Array<>();
@@ -27,10 +31,18 @@ public class PlayerActor extends CreatureActor {
     private Equipment accessories = new Equipment(Equipment.EquipmentType.NONE);
     private boolean inDialogue = false;
 
+    /**
+     * constructor of PlayerActor
+     * @param region texture rigion
+     */
     public PlayerActor(TextureRegion region) {
         super(region);
     }
 
+    /**
+     * constructor of PlayerActor
+     * @param mapObject map object of player
+     */
     public PlayerActor(MapObject mapObject) {
         super(mapObject);
         weapon.attackDamage = this.attackDamage;
@@ -38,25 +50,37 @@ public class PlayerActor extends CreatureActor {
         weapon.attackSpeed = this.attackSpeed;
     }
 
+    /**
+     * act method of player actor
+     * @param delta time interval between ticks
+     */
     @Override
     public void act(float delta) {
         super.act(delta);
         lastPosition.x = this.getX();
         lastPosition.y = this.getY();
+        // move method of actor
         if (!inBattle && !inDialogue) {
+            // move up
             if (Gdx.input.isKeyJustPressed(Input.Keys.K) ||
                     Gdx.input.isKeyJustPressed(Input.Keys.UP))//up
             {
                 sprite.setY(((int) sprite.getY()) + 32);
-            } else if (Gdx.input.isKeyJustPressed(Input.Keys.J) ||
+            }
+            // move down
+            else if (Gdx.input.isKeyJustPressed(Input.Keys.J) ||
                     Gdx.input.isKeyJustPressed(Input.Keys.DOWN))//down
             {
                 sprite.setY(((int) sprite.getY()) - 32);
-            } else if (Gdx.input.isKeyJustPressed(Input.Keys.H) ||
+            }
+            // move left
+            else if (Gdx.input.isKeyJustPressed(Input.Keys.H) ||
                     Gdx.input.isKeyJustPressed(Input.Keys.LEFT))//left
             {
                 sprite.setX(((int) sprite.getX()) - 32);
-            } else if (Gdx.input.isKeyJustPressed(Input.Keys.L) ||
+            }
+            // move right
+            else if (Gdx.input.isKeyJustPressed(Input.Keys.L) ||
                     Gdx.input.isKeyJustPressed(Input.Keys.RIGHT))//right
             {
                 sprite.setX(((int) sprite.getX()) + 32);
@@ -64,21 +88,33 @@ public class PlayerActor extends CreatureActor {
         }
     }
 
+    /**
+     * death action of player
+     */
     @Override
     public void takeDeath() {
         if (Assets.isOpenSound())
             Assets.deathSound.play();
     }
 
+    /**
+     * undo player move action
+     */
     public void undoAct() {
         this.setX(lastPosition.x);
         this.setY(lastPosition.y);
     }
 
+    /**
+     * pick up props method
+     * @param props
+     */
     public void pickUp(Props props) {
+        // if sound option is open, play pick up sound
         if (Assets.isOpenSound())
             Assets.pickSound.play();
 
+        // if props type is equipment, equip it
         if (props.propsType == PropsActor.PropsType.EQUIPMENT) {
             Equipment equipment = (Equipment) props;
             switch (equipment.equipmentType) {
@@ -99,21 +135,34 @@ public class PlayerActor extends CreatureActor {
                     break;
             }
             updateAttributes();
-        } else if (props.propsType == PropsActor.PropsType.POTION) {
+        }
+        // if props is potion, use it
+        else if (props.propsType == PropsActor.PropsType.POTION) {
             this.usePotion((Potion) props);
-        } else if (props.propsType == PropsActor.PropsType.KEY) {
+        }
+        // if props is key, put it into backpack
+        else if (props.propsType == PropsActor.PropsType.KEY) {
             this.backpack.add(props);
         }
     }
 
+    /**
+     * use potion to recover health point and magic point
+     * @param potion potion to use
+     */
     public void usePotion(Potion potion) {
         this.healthPoint -= potion.healthConsume;
         this.magicPoint -= potion.magicConsume;
     }
 
+    /**
+     * take attack method of player
+     * attack speed base on player attack speed
+     */
     @Override
     public void takeAttack() {
         long now = TimeUtils.nanoTime();
+        // if there are not time to attack, player attack animation
         if (now - lastAttack > 1000000000 / attackSpeed && !this.isDead() && opponent != null) {
             this.lastAttack = now;
             float healthConsume = this.weapon.healthConsume;
@@ -127,7 +176,9 @@ public class PlayerActor extends CreatureActor {
                 Assets.attackSound.play();
 
             float realDamage = this.opponent.takeDamage(this.attackDamage, this.abilityPower, this);
-        } else if (now - lastAttack > 0 && opponent != null) {
+        }
+        // play attack animaiton
+        else if (now - lastAttack > 0 && opponent != null) {
             float s = (now - lastAttack) / (1000000000 / attackSpeed);
             this.setX((float) (posBeforeBattle.x + (opponent.getX() - posBeforeBattle.x) * s * 0.5));
             this.setY((float) (posBeforeBattle.y + (opponent.getY() - posBeforeBattle.y) * s * 0.5));
@@ -135,10 +186,18 @@ public class PlayerActor extends CreatureActor {
 
     }
 
+    /**
+     * dropdown props method
+     * @param props
+     */
     public void dropDown(Props props) {
         backpack.removeValue(props, false);
     }
 
+    /**
+     * equip equipment
+     * @param equipment
+     */
     public void equip(Equipment equipment) {
         if (Assets.isOpenSound())
             Assets.equipSound.play();
@@ -165,21 +224,39 @@ public class PlayerActor extends CreatureActor {
         updateAttributes();
     }
 
+    /**
+     * take dialogue with npc
+     * @param actor npc actor that player talk with
+     * @param dialogueJson dialogue json object
+     */
     public void takeDialogue(NpcActor actor, JSONObject dialogueJson) {
+        // if player is in dialogue, stop taking another dialogue
         if (!inDialogue) {
             this.inDialogue = true;
             actor.takeDialogue(this.getStage(), this, dialogueJson);
         }
     }
 
+    /**
+     * judge if player is in dialogue
+     * @return return player if in dialogue
+     */
     public boolean inDialogue() {
         return inDialogue;
     }
 
+    /**
+     * over the dialogue
+     * call when dialogue is over
+     */
     public void overDialogue() {
         this.inDialogue = false;
     }
 
+    /**
+     * update player attributes
+     * call when player equip equipment
+     */
     private void updateAttributes() {
         this.attackSpeed = weapon.attackSpeed;
         this.attackDamage = weapon.attackDamage;
