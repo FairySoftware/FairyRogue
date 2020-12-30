5ddedbd7288137bb402ceb79586a58cd4a9e23dc
ColourfulBlack <kalicock@outlook.com>
2020-12-30 13:43:45 +0800

    add main screen commant

--- core/src/org/fairysoftw/fairyrogue/screen/MainScreen.java
+++ core/src/org/fairysoftw/fairyrogue/screen/MainScreen.java
@@ -11,11 +11,26 @@ import org.fairysoftw.fairyrogue.FairyRogue;
 import org.fairysoftw.fairyrogue.actor.PlayerActor;
 import org.fairysoftw.fairyrogue.stage.GameStage;
 
+/** {@inheritDoc}
+ * main screen that display the main game scene
+ * will be change to next main screen every time clear a stage
+ * will be change to setting screen when press esc
+ * will be change to start screen when player died
+ */
 public class MainScreen extends ScreenAdapter {
+    // current game stage
     private GameStage stage;
+    // the major Game class
     private Game game;
+    // the map array store next maps
     private Array<TiledMap> maps;
 
+    /**
+     * main screen constructor
+     * @param game major game class instance
+     * @param maps the map array store next maps
+     * @param lastPlayerActor last stage player
+     */
     public MainScreen(Game game, Array<TiledMap> maps, PlayerActor lastPlayerActor) {
         this.game = game;
         this.maps = maps;
@@ -23,51 +38,89 @@ public class MainScreen extends ScreenAdapter {
         maps.removeIndex(0);
     }
 
+    /**
+     * set game to next main screen
+     * will be call when this stage is clear
+     */
     private void nextStage() {
         game.setScreen(new MainScreen(game, maps, this.stage.getPlayerActor()));
     }
 
+    /**
+     * clear game
+     * will be call when player arrived clear point
+     */
     private void gameClear() {
         game.setScreen(new WinScreen((FairyRogue) game));
     }
 
+    /**
+     * over the game
+     * will be call when player died
+     */
     public void gameOver() {
         game.setScreen(new DeathScreen((FairyRogue) game));
     }
 
+    /** {@inheritDoc}
+     * will be called every this screen shows
+     */
     @Override
     public void show() {
         stage.draw();
     }
 
+    /**
+     * render screen method
+     * will be called every tick
+     * @param delta time interval between ticks
+     */
     @Override
     public void render(float delta) {
+        // if stage is clear and there are no map, the game is clear, show clear image
         if (stage.isClear() && maps.size == 0) {
             this.gameClear();
-        } else if (stage.isClear()) {
+        }
+        // if stage is clear, switch to next stage
+        else if (stage.isClear()) {
             nextStage();
             this.stage.dispose();
             return;
-        } else if (stage.isGameOver()) {
+        }
+        // if game over, show game over image
+        else if (stage.isGameOver()) {
             gameOver();
             return;
         }
         Gdx.gl.glClearColor(0, 0, 0, 1.0f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        // call stage action
         stage.act();
+        // call stage drawing
         stage.draw();
+        // if esc is pressed, show setting screen
         if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
             game.setScreen(new PauseScreen((FairyRogue) game));
         }
     }
 
+    /**
+     * resize the window
+     * @param width window width
+     * @param height windows height
+     */
     @Override
     public void resize(int width, int height) {
         stage.resize(width, height);
     }
 
+    /**
+     * dispose method, won't call automatically
+     * please call manually
+     */
     @Override
     public void dispose() {
+        // dispose current game stage
         stage.dispose();
     }
 
